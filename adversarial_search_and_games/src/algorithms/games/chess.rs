c95da630de754aea90f5228f0df1e7b78e2b3b7f
mabeh19 <mathiasibehrens@gmail.com>
2022-08-04 12:51:29 +0200

    Fixed minor issues

--- adversarial_search_and_games/src/algorithms/games/chess.rs
+++ adversarial_search_and_games/src/algorithms/games/chess.rs
@@ -178,6 +178,7 @@ impl ChessGame {
         if piece_taken {
             Self::get_other_player_as_mut(&mut self.board).remove_piece(&action);
         }
+        self.board.3[self.board.2 % self.board.3.len()] = self.board.0.clone();
         self.board.2 += 1;
     }
 
@@ -192,10 +193,9 @@ impl ChessGame {
         }
         let state_copy = new_state.clone();
         Self::get_other_player_as_mut(&mut new_state).check_if_checked(&state_copy, Self::get_current_player(&state_copy));
-        new_state.2 += 1;
-        
         new_state.3[state.2 % state.3.len()] = new_state.0.clone();
 
+        new_state.2 += 1;
         new_state
     }
 
